67600c7725fc7553a463aefd3799453117ccb791
trungnl2000 <trungnl2000@gmail.com>
2024-05-18 22:03:20 +0200

    fix script dlv3 svd

--- scripts/dlv3/hosvd/hosvd_dlv3_234.sh
+++ scripts/dlv3/hosvd/hosvd_dlv3_234.sh
@@ -1,4 +1,4 @@
-# Cityscapes->VOC12Aug, Train last 5 layers in DeepLabV3-ResNet18 with gradient filter
+# Cityscapes->VOC12Aug, Train last 5 layers in DeepLabV3-ResNet18 with HOSVD
 python train.py configs/deeplabv3/hosvd_5L_deeplabv3_r18-d8_512x512_20k_voc12aug.py --load-from calib/calib_deeplabv3_r18-d8_512x512_1k_voc12aug_cityscapes/version_0/latest.pth --cfg-options data.samples_per_gpu=8 --seed 234
-# Cityscapes->VOC12Aug, Train last 10 layers in DeepLabV3-ResNet18 with gradient filter
+# Cityscapes->VOC12Aug, Train last 10 layers in DeepLabV3-ResNet18 with HOSVD
 python train.py configs/deeplabv3/hosvd_10L_deeplabv3_r18-d8_512x512_20k_voc12aug.py --load-from calib/calib_deeplabv3_r18-d8_512x512_1k_voc12aug_cityscapes/version_0/latest.pth --cfg-options data.samples_per_gpu=8 --seed 234
