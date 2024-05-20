c460c42535d3810573f7121059089c38556f7c89
trungnl2000 <trungnl2000@gmail.com>
2024-05-20 21:03:29 +0200

    chỉnh lại variance của fcn

--- configs/fcn/0.8/hosvd_10L_fcn_r18-d8_512x512_voc12aug.py
+++ configs/fcn/0.8/hosvd_10L_fcn_r18-d8_512x512_voc12aug.py
@@ -9,12 +9,12 @@ freeze_layers = [
 hosvd_var = dict(
     enable=True,
     filter_install=[
-        dict(path="decode_head.conv_cat", type='cbr', SVD_var=0.9),
-        dict(path="decode_head.convs.0", type='cbr', SVD_var=0.9),
-        dict(path="decode_head.convs.1", type='cbr', SVD_var=0.9),
-        dict(path="backbone.layer4.1", type='resnet_basic_block', SVD_var=0.9),
-        dict(path="backbone.layer4.0", type='resnet_basic_block', SVD_var=0.9),
-        dict(path="backbone.layer4.0.downsample.0", type='conv', SVD_var=0.9),
-        dict(path="backbone.layer3.1", type='resnet_basic_block', SVD_var=0.9),
+        dict(path="decode_head.conv_cat", type='cbr', SVD_var=0.8),
+        dict(path="decode_head.convs.0", type='cbr', SVD_var=0.8),
+        dict(path="decode_head.convs.1", type='cbr', SVD_var=0.8),
+        dict(path="backbone.layer4.1", type='resnet_basic_block', SVD_var=0.8),
+        dict(path="backbone.layer4.0", type='resnet_basic_block', SVD_var=0.8),
+        dict(path="backbone.layer4.0.downsample.0", type='conv', SVD_var=0.8),
+        dict(path="backbone.layer3.1", type='resnet_basic_block', SVD_var=0.8),
     ]
 )
--- configs/fcn/0.8/hosvd_5L_fcn_r18-d8_512x512_voc12aug.py
+++ configs/fcn/0.8/hosvd_5L_fcn_r18-d8_512x512_voc12aug.py
@@ -7,10 +7,10 @@ freeze_layers = [
 hosvd_var = dict(
     enable=True,
     filter_install=[
-        dict(path="decode_head.conv_cat", type='cbr', SVD_var=0.9),
-        dict(path="decode_head.convs.0", type='cbr', SVD_var=0.9),
-        dict(path="decode_head.convs.1", type='cbr', SVD_var=0.9),
-        dict(path="backbone.layer4.1", type='resnet_basic_block', SVD_var=0.9),
+        dict(path="decode_head.conv_cat", type='cbr', SVD_var=0.8),
+        dict(path="decode_head.convs.0", type='cbr', SVD_var=0.8),
+        dict(path="decode_head.convs.1", type='cbr', SVD_var=0.8),
+        dict(path="backbone.layer4.1", type='resnet_basic_block', SVD_var=0.8),
     ]
 )
 
